197f0e66396493abac2dd6d8c17087e7c58e24b7
gnnorton2 <161044764+gnnorton2@users.noreply.github.com>
2024-03-27 19:56:09 -0400

    slideCatch v1.1
    milestones 1-3 finished
    background works, sprite works and in position

--- slideCatch/fillTheCup.py
+++ slideCatch/fillTheCup.py
@@ -7,47 +7,23 @@ Created on Tue Mar 26 10:59:25 2024
 
 import pygame, random, simpleGE
 
-def main():
-    
-    
-    #Important and Initialize Idea
-    #already imported
-    pygame.init()
-    
-    #iDea Display
-    screen = pygame.display.set_mode((640, 480))
-    pygame.display.set_caption("Fill the Cup")
-    
-    #idEa background
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background = pygame.image.load("bubbles.png")
-    
-    clock = pygame.time.Clock()
-    keepGoing = True
-    
-    #aLter loop
-    while keepGoing:
-        
-        #alTer Timer
-        clock.tick(30) #fps
-        
-        #altEr Event haandling
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                keepGoing = False
-    
-    #alteR refresh
-        screen.blit(background, (0, 0))
-        pygame.display.flip()
-    
 class EcDonalds(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
-        self.setImage("youveGotMail") #CHANGE THIS LATER ONCE WORKING
-        self.setSize(50, 50)
-        
+        self.setImage("cup.png")
+        self.position = (320, 400)
 
+class Game(simpleGE.Scene):
+    def __init__(self):
+        super().__init__()
+        self.setImage("bubbles.png")
+        self.cup = EcDonalds(self)
+        
+        self.sprites = [self.cup]
+        
+def main():
+    game = Game()
+    game.start()
 
 if __name__ == "__main__":
     main()
