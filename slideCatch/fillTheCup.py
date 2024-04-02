2a0f5cc2a33796fbfd38507c9293d856885aaf99
gnnorton2 <161044764+gnnorton2@users.noreply.github.com>
2024-04-02 14:29:59 -0400

    CatchSlide v.2.2
    Milestone 8 almost complete. Added intro screen and last score. Last score currently in progress. 13:15

--- slideCatch/fillTheCup.py
+++ slideCatch/fillTheCup.py
@@ -96,9 +96,73 @@ class Game(simpleGE.Scene):
             print(f"Score: {self.score}")
             self.stop()
     
+class Instructions(simpleGE.Scene):
+    def __init__(self):
+        super().__init__()
+        self.setImage("bubbles.png")
+        
+        self.response = "Quit"
+        self.pastScore = 0
+        self.howTo = simpleGE.MultiLabel()
+        self.howTo.textLines = [
+            "You just got a job at EcDonald's.",
+            "Somebody mixed up all the spicy water.",
+            "",
+            "Move with the left and right arrow keys",
+            "to fill the customer's cup with",
+            "the correct spicy water.",
+            "",
+            "Here at EcDonald's, we Sprit, Ciera Missed,",
+            "7 Down, but NEVER Mountain Don't.",
+            "",
+            "Catch as much crisp spicy water as you can",
+            "before time runs out."
+            ]
+        self.howTo.center = (320, 215)
+        self.howTo.size = (600, 400)
+        
+        self.btnPlay = simpleGE.Button()
+        self.btnPlay.text = "Play"
+        self.btnPlay.center = (100, 450)
+        
+        self.btnQuit = simpleGE.Button()
+        self.btnQuit.text = "Quit"
+        self.btnQuit.center = (540, 450)
+        
+        self.pastScoreLabel = simpleGE.Label()
+        self.pastScoreLabel.text = "Last Score: 0"
+        self.pastScoreLabel.center = (320, 450)
+    
+        
+        
+        self.sprites = [self.howTo,
+                        self.btnPlay,
+                        self.btnQuit,
+                        self.pastScoreLabel]
+        
+    def process(self):
+        if self.btnPlay.clicked:
+            self.response = "Play"
+            self.stop()
+            
+        if self.btnQuit.clicked:
+            self.response = "Quit"
+            self.stop()
+    
+    
 def main():
-    game = Game()
-    game.start()
+    keepGoing = True
+    while keepGoing:
+        
+        instructions = Instructions()
+        instructions.start()
+        print(instructions.response)
+    
+        if instructions.response == "Play":    
+            game = Game()
+            game.start()
+        else:
+            keepGoing = False
 
 if __name__ == "__main__":
     main()
